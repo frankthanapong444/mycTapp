69b85587d1a1aef6e89af3b0ce5e546302bcb758
frang <franghero888@gmail.com>
2022-07-27 18:49:35 +0700

    first commit

---
+++ myappi/android/app/src/main/kotlin/com/example/myappi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myappi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ myappi/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    title: "my App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Helloflutter "),
+      ),
+    ),
+  );
+  runApp(app);
+}
